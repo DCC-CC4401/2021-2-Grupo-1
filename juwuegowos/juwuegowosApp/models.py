5d6035850de1471a7e35ce2fd44c69dfb36a0670
unknown <gago3110@gmail.com>
2021-09-24 19:39:25 -0300

    modelos actualizados para usar taggit

--- juwuegowos/juwuegowosApp/models.py
+++ juwuegowos/juwuegowosApp/models.py
@@ -3,6 +3,7 @@ from django.db.models.fields import CharField
 from django.db.models.fields.related import ForeignKey
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -11,27 +12,18 @@ class User(AbstractUser):
 
 class Game(models.Model):
     name = models.CharField(max_length=255)
+    developer = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=51000)
     date = models.DateField(default=timezone.now().strftime("%d/%m/%Y"))
     nsfw = models.BooleanField(default=False)
     thumbnail = models.ImageField(upload_to="images/thumbnails")
+    tags = TaggableManager()
 
 class Comment(models.Model):
     user_id = ForeignKey(User, on_delete=models.CASCADE)
     game_id = ForeignKey(Game, on_delete=models.CASCADE)
     comment = models.CharField(max_length=51000)
     date = models.DateTimeField(timezone.now().strftime("%d/%m/%Y - %H:%M"))
-
-class Tag(models.Model):
-    tag = models.CharField(max_length=50, primary_key=True)
-
-class GameTag(models.Model):
-    game_id = ForeignKey(Game, on_delete=models.CASCADE)
-    tag = ForeignKey(Tag, on_delete=models.CASCADE)
-
-class GameDeveloper(models.Model):
-    user_id = ForeignKey(User, on_delete=models.CASCADE)
-    game_id = ForeignKey(Game, on_delete=models.CASCADE)
 
 class SocialMedia(models.Model):
     name = CharField(max_length=32)
